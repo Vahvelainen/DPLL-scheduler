56cf3580e5345aeab30703dc834353d8c470364b
vahvake <62074587+Vahvelainen@users.noreply.github.com>
2023-11-15 21:24:34 +0100

    Add rows for resources

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,23 @@
 <script lang="ts">
-  
+  import TimeRow from "./TimeRow.svelte";
+
+  //First just name everything with strings
+  let resources : Array<String>  = [
+    'Resource A',
+    'Resource B',
+    'Resource C',
+  ]
+
+  //Number of timeslots, could be adjustable
+  const t_num = 7
+
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>DPLL scheduler</h1>
+<p>Arrange the use of resources</p>
+
+{#each resources as resource, i}
+  <TimeRow label={resource} slots={t_num}>
+    <!-- The time reservations ought to be displayed here -->
+  </TimeRow>
+{/each}
---
+++ src/routes/TimeRow.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+
+  //First just name everything with strings
+  export let label :String
+  export let slots :Number
+
+</script>
+
+<div class="row" >
+
+  <div class="labelBox">
+    <span>{label}</span>
+  </div>
+
+  <div class="slots">
+    
+    <!-- Slots are for visual reference. Bars will be relative to parent slots. Well wrry about indexin them later -->
+    {#each Array(slots) as _, i }
+      <div class={ 'slot ' +  ( i % 2 ? 'dark' : 'light' ) } style="width: calc( 100% / {slots} );"></div>
+    {/each}
+
+    <!-- For showing the time bars -->
+    <slot/>
+
+  </div>
+
+</div>
+
+<style>
+  .row {
+    display: flex;
+    width: 100%;
+    border: rgba(1, 1, 1, 0) 2px dashed;
+  }
+
+  .row:hover {
+    border: blue 2px solid;
+  }
+
+  .labelBox{
+    width: 10em;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    padding-left: 1em;
+  }
+
+  .slots {
+    display: flex;
+    width: 100%;
+  }
+
+  .slot {
+    height: 2.5em;
+  }
+
+  .light{
+    background-color: #D9D9D9;
+  }
+
+  .dark{
+    background-color: #E6F7FF;
+  }
+</style>
