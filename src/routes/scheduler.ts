bff5e727e362e5239ff983bb3b5200d76ee2f7ba
vahvake <62074587+Vahvelainen@users.noreply.github.com>
2023-11-20 00:32:04 +0100

    Refactor scheduler

--- src/routes/scheduler.ts
+++ src/routes/scheduler.ts
@@ -1,17 +1,36 @@
 
 import type {Formula, Clause, Literal, Assignment} from './dpll'
 import dpll from './dpll'
+import {ATOM, AND, OR, NOT, XOR, IMPL, EQVI} from './logic'
 
 import type {Project, Step} from './types'
 
-export default function solveSchedule(projects: Project[], resources: string[], timeSteps: number = 2 ): Literal[] {
-  let formula: Formula = [] 
-  //Iteratively add timesteps to the process
-  //Parse clauses based on the stuff
-  //Follow structure of `Resource${resource}_Time${time}_Project${project.name}_Step${step.name}`
-  // Maybe come up with better separator than underscore
+export default function solveSchedule(projects: Project[], resources: string[], timeSteps: number = 1): Literal[] {
 
-  // 1. Every step has to happen (lets start with once)
+  // Parse clauses based on the rules
+  // Variables follow structure of `Resource${resource}_Time${time}_Project${project.name}_Step${step.name}`
+  // Maybe come up with better separator than underscore in the future
+
+  const formula = AND([
+    everyStepHappens(projects, resources, timeSteps),
+    noResourceOverlap(projects, resources, timeSteps),
+    followProjectOrder(projects, resources, timeSteps),
+    // stepsLegthsMatch(projects, resources, timeSteps),
+  ])
+
+  //In the end in the results, only clauses stated to be true ought to be displayd and analysed
+  let satisfiableFromula = dpll(formula)
+  if ( satisfiableFromula ) {
+    return filterResults(satisfiableFromula)
+  } else {
+    // Iteratively add timesteps to the process
+    return solveSchedule(projects, resources, timeSteps + 1  )
+  }
+}
+
+function everyStepHappens(projects: Project[], resources: string[], timeSteps: number): Formula {
+  let formula: Formula = [] 
+  // Every step has to happen
   // Example: If step1 of project needs to happen on right resource on any of the times
   // p \/ q 
   // [ 'Resource1_Time1_ProjectA_Step1', 'Resource1_Time2_ProjectA_Step1', ...for every step ]
@@ -25,7 +44,11 @@ export default function solveSchedule(projects: Project[], resources: string[], 
       formula.push(clause)
     }
   }
+  return formula
+}
 
+function noResourceOverlap(projects: Project[], resources: string[], timeSteps: number): Formula {
+  let formula: Formula = [] 
   // 2. Resource can only be used for one thing at a time
   // Example: If resource1 is used for project1 at t1, it is not used for any other steps at the same time
   // Logical equivalence: p => -q === -p \/ -q
@@ -33,15 +56,15 @@ export default function solveSchedule(projects: Project[], resources: string[], 
   // Repeated for all the step combinastions for timestep and resource. The clauses need to make surethat there is n-1 false assigments for the timestep
   //For each timestep... 
   for (let time = 0; time < timeSteps; time++) {
-    let allSteps: Literal[] = []
+    let stepLiterals: Literal[] = []
     for ( const project of projects ) {
       for (const step of project.steps) {
-        allSteps.push( `Resource${step.resource}_Time${time}_Project${project.name}_Step${step.name}` )
+        stepLiterals.push( `Resource${step.resource}_Time${time}_Project${project.name}_Step${step.name}` )
       }
     }
     //For each resource...
     for (const resource of resources) {
-      const resourceSteps = allSteps.filter( step => step.includes(`Resource${resource}_`) )
+      const resourceSteps = stepLiterals.filter( step => step.includes(`Resource${resource}_`) )
       //For each combination... 
       for (let i = 0; i < resourceSteps.length; i++) {
         for (let j = i + 1; j < resourceSteps.length; j++) {
@@ -51,7 +74,11 @@ export default function solveSchedule(projects: Project[], resources: string[], 
       }
     }
   }
+  return formula
+}
 
+function followProjectOrder(projects: Project[], resources: string[], timeSteps: number): Formula {
+  let formula: Formula = [] 
   // 3. Steps have to happen in right order
   // Example: If step1 of project is happening at t2, later steps of the same project arent happening in previous times
   // Logical equivalence: p => -q === -p \/ -q
@@ -77,26 +104,25 @@ export default function solveSchedule(projects: Project[], resources: string[], 
       }
     }
   }
+  return formula
+}
 
-  // 5. Long steps need to have the subsequent times for resources
+function stepsLegthsMatch(projects: Project[], resources: string[], timeSteps: number): Formula {
+  // Long steps need to have the subsequent times for resources
   // If step is happening in t2 it needs to happen in t1 or t2 (and all the combinastions for longer ones)
   // q => ( y /\ z )
   // This is hard man, it gets added last
+  return []
+}
 
-  //In the end in the results, only clauses stated to be true ought to be displayd and analysed
-
-  let satisfiableFromula = dpll(formula)
-  if ( satisfiableFromula ) {
-    let filteredResult: Literal[] = []
-    for ( const literal in satisfiableFromula) {
-      if ( satisfiableFromula[literal] ) {
-        filteredResult.push(literal)
-      }
+function filterResults(formula: Assignment): Literal[] {
+  let result: Literal[] = []
+  for ( const literal in formula) {
+    if ( formula[literal] ) {
+      result.push(literal)
     }
-    return filteredResult
   }
-
-  return solveSchedule(projects, resources, timeSteps + 1  )
+  return result
 }
 
 function getCombinations(arr: Array<any>): Array< Array<any> > {
